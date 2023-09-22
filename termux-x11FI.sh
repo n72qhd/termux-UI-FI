770b42ce5a9f47ad403e14e93ab18c8767b0f562
n72qhd <145501666+n72qhd@users.noreply.github.com>
2023-09-22 22:49:56 +0300

    Update termux-x11FI.sh

--- termux-x11FI.sh
+++ termux-x11FI.sh
@@ -2,7 +2,7 @@
 apt update
 apt install termux-x11-nightly xfce4 firefox ffmpeg goffice vlc
 
-wget https://github.com/n72qhd/termux-UI-FI/blob/main/startx.sh
+wget https://github.com/n72qhd/termux-UI-FI/releases/download/A/startx.sh
 chmod +x startx.sh
 echo Installation is complete. You can start XORG by typing "./starx.sh" in your console. Have a nice day.
 
